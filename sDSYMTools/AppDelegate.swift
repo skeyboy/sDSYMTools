ece47340a7c82fc2b3e77f421731a6a4845a9433
李雨龙 <sk@liyulongdeMacBook-Pro.local>
2018-05-03 14:41:43 +0800

    Initial Commit

---
+++ sDSYMTools/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  sDSYMTools
+//
+//  Created by 李雨龙 on 2018/5/3.
+//  Copyright © 2018年 李雨龙. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ sDSYMTools/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sDSYMTools
+//
+//  Created by 李雨龙 on 2018/5/3.
+//  Copyright © 2018年 李雨龙. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
